cbd53f78a77986203ada57879e796ba07992a6d2
Miky <migueldah91@gmail.com>
2017-10-08 02:49:35 +0200

    limitadores de camara

---
+++ Talentum/Assets/cameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    float speed = 8f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,transform.position.y, transform.position.z);
+	}
+}
