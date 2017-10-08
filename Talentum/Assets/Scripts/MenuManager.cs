29035710bda0bcc45e9dca2c2b853b78793e52e1
Miky <migueldah91@gmail.com>
2017-10-08 03:49:23 +0200

    Main Menu y cambios de escenas

---
+++ Talentum/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject menu;
+    [SerializeField]
+    GameObject controls;
+    [SerializeField]
+    GameObject animControls;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void showControls()
+    {
+        controls.SetActive(true);
+    }
+
+    public void newGame()
+    {
+        SceneManager.LoadSceneAsync("pruebaProcedural");
+    }
+    public void closeControls()
+    {
+        controls.SetActive(false);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
