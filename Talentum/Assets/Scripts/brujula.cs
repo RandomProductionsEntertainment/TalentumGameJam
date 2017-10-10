29453474ab4859345a029213587804d99e5ffd8e
AzureAD\BlancaMacazagaZuazo <blancamacazaga@gmail.com>
2017-10-10 07:57:42 +0200

    feedback text

--- Talentum/Assets/Scripts/brujula.cs
+++ Talentum/Assets/Scripts/brujula.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class brujula : MonoBehaviour {
 
@@ -8,19 +9,48 @@ public class brujula : MonoBehaviour {
     bool finished = true;
     float timer = 0;
     int changeDir = 0;
-
+    public Text north;
+    public Text south;
+    public Text east;
+    public Text west;
 
     // Use this for initialization
     void Start()
     {
+        north.enabled = false;
+        south.enabled = false;
+        east.enabled = false;
+        west.enabled = false;
 
-     
     }
+    private void OnGUI()
+    {
+        if (north.enabled && timer > 3)
+        {
+            north.enabled = false;
 
+        }
+        else
+        {
+            north.text = "Back to ANORMAL!";
+        }
+        if (south.enabled && timer > 3)
+            south.enabled = false;
+        else
+            south.text = "Now UP is DOWN!";
+        if (east.enabled && timer > 3)
+            east.enabled = false;
+        else
+            east.text = "RUN and you'll JUMP!";
+        if (west.enabled && timer > 3)
+            west.enabled = false;
+        else
+            west.text = "NORTH to the WEST!";
+    }
     // Update is called once per frame
     void Update()
     {
-        if (timer >10)
+        if (timer > 10)
         {
 
             if (finished)
@@ -29,28 +59,40 @@ public class brujula : MonoBehaviour {
                 finished = false;
             }
 
- 
-            if(angle < changeDir)
+
+            if (angle < changeDir)
             {
                 angle += 1;
-                transform.Rotate(new Vector3(0, 0, 1),changeDir * Time.deltaTime);
+                transform.Rotate(new Vector3(0, 0, 1), changeDir * Time.deltaTime);
             }
             if (angle == changeDir)
             {
                 if (angle >= -45 && angle <= 45 || angle >= 315 && angle <= 405 || angle <= -315 && angle >= -405)
+                {
                     GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ChangeDirection(0);
+                    north.enabled = true;
+                }
                 else if (angle <= 135 && angle >= 45 || angle >= -315 && angle <= -225)
+                {
                     GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ChangeDirection(3);
+                    west.enabled = true;
+                }
                 else if (angle >= 135 && angle <= 225 || angle >= -225 && angle <= -135)
+                {
                     GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ChangeDirection(2);
+                    south.enabled = true;
+                }
                 else if (angle <= 315 && angle >= 225 || angle >= -135 && angle <= -45)
+                {
                     GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ChangeDirection(1);
+                    east.enabled = true;
+                }
                 angle = 0;
                 finished = true;
                 timer = 0;
             }
 
-            
+
         }
         else
             timer += Time.deltaTime;
